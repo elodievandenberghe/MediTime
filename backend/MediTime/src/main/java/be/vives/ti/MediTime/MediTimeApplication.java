efcf2b4d9486098795ef47e36a726873062a7e48
Wout Vandenberghe <wout.vandenberghe4@student.vives.be>
2024-11-23 14:27:00 +0100

    setting up directories

---
+++ backend/MediTime/src/main/java/be/vives/ti/MediTime/MediTimeApplication.java
@@ -0,0 +1,12 @@
+package be.vives.ti.MediTime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediTimeApplication.class, args);
+	}
+}
---
+++ backend/MediTime/src/test/java/be/vives/ti/MediTime/MediTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vives.ti.MediTime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
